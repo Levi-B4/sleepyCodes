8c7547a3ac516ac6e146d04e9bd9ca6aa138a40d
Levi S23 <levi.butler4@gmail.com>
2026-07-10 08:50:50 -0400

    added jsoncpp dependency

--- rankings/main.cpp
+++ rankings/main.cpp
@@ -1,12 +1,20 @@
 #include "ranker.cpp"
 #include "ftxTest.cpp"
 
+#include <json/json.h>
 
 #include <iostream>
+#include <fstream>
+
+
 
 using std::cout, std::endl;
 
 int main(){
+	Json::Value root;
+	std::ifstream config_doc("config_doc.json", std::ifstream::binary);
+	config_doc >> root;
+	
 	testing();
 	Ranker r;
 	r.read();
